d1ecad8dc5532ac892ba4e76422cf212d2bfeca0
glassesboyy <aliperwira26@gmail.com>
2025-04-19 19:44:50 +0700

    perbarui warna spotlight menjadi biru dan ungu, lalu menyempurnakan breakpoint pada tulisan h1 di halaman hero

--- src/components/page/hero.jsx
+++ src/components/page/hero.jsx
@@ -5,14 +5,11 @@ export const Hero = () => {
   return (
     <div className="h-screen w-full flex md:items-center md:justify-center bg-black antialiased bg-grid-white relative overflow-hidden">
       <Spotlight />
-      <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-32 md:pt-20">
+      <div className="p-4 max-w-5xl md:max-w-3xl lg:max-w-5xl mx-auto relative z-10 w-full pt-32 md:pt-20">
         <h1 className="text-5xl md:text-6xl lg:text-8xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-white via-gray-100 to-black/5">
-          Start and Build
-          <br className="block sm:hidden" />
-          Your Crypto
-          <br className="block sm:hidden" />
-          Portfolio Here
-          <br className="hidden sm:block" />
+          Start and Build <br className="block sm:hidden" />
+          Your Crypto <br className="block sm:hidden" />
+          Portfolio Here <br className="hidden sm:block" />
         </h1>
         <p className="mt-2  md:mt-4 font-light text-sm md:text-lg lg:text-xl text-white max-w-5xl text-center mx-auto px-4 md:px-8">
           Build your dream crypto portfolio and discover winning strategies to
--- src/components/ui/spotlight-new.jsx
+++ src/components/ui/spotlight-new.jsx
@@ -1,20 +1,19 @@
-"use client";;
-import React from "react";
+"use client";
 import { motion } from "motion/react";
 
 export const Spotlight = ({
-  gradientFirst = "radial-gradient(68.54% 68.72% at 55.02% 31.46%, hsla(210, 100%, 85%, .08) 0, hsla(210, 100%, 55%, .02) 50%, hsla(210, 100%, 45%, 0) 80%)",
-  gradientSecond = "radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .06) 0, hsla(210, 100%, 55%, .02) 80%, transparent 100%)",
-  gradientThird = "radial-gradient(50% 50% at 50% 50%, hsla(210, 100%, 85%, .04) 0, hsla(210, 100%, 45%, .02) 80%, transparent 100%)",
+  gradientFirst = "radial-gradient(68.54% 68.72% at 55.02% 31.46%, rgba(147, 51, 234, 0.08) 0%, rgba(79, 70, 229, 0.02) 50%, rgba(67, 56, 202, 0) 80%)",
+  gradientSecond = "radial-gradient(50% 50% at 50% 50%, rgba(147, 51, 234, 0.06) 0%, rgba(79, 70, 229, 0.02) 80%, transparent 100%)",
+  gradientThird = "radial-gradient(50% 50% at 50% 50%, rgba(147, 51, 234, 0.04) 0%, rgba(79, 70, 229, 0.02) 80%, transparent 100%)",
   translateY = -350,
   width = 560,
   height = 1380,
   smallWidth = 240,
   duration = 7,
-  xOffset = 100
+  xOffset = 100,
 } = {}) => {
   return (
-    (<motion.div
+    <motion.div
       initial={{
         opacity: 0,
       }}
@@ -24,7 +23,8 @@ export const Spotlight = ({
       transition={{
         duration: 1.5,
       }}
-      className="pointer-events-none absolute inset-0 h-full w-full">
+      className="pointer-events-none absolute inset-0 h-full w-full"
+    >
       <motion.div
         animate={{
           x: [0, xOffset, 0],
@@ -35,7 +35,8 @@ export const Spotlight = ({
           repeatType: "reverse",
           ease: "easeInOut",
         }}
-        className="absolute top-0 left-0 w-screen h-screen z-40 pointer-events-none">
+        className="absolute top-0 left-0 w-screen h-screen z-40 pointer-events-none"
+      >
         <div
           style={{
             transform: `translateY(${translateY}px) rotate(-45deg)`,
@@ -43,7 +44,8 @@ export const Spotlight = ({
             width: `${width}px`,
             height: `${height}px`,
           }}
-          className={`absolute top-0 left-0`} />
+          className={`absolute top-0 left-0`}
+        />
 
         <div
           style={{
@@ -52,7 +54,8 @@ export const Spotlight = ({
             width: `${smallWidth}px`,
             height: `${height}px`,
           }}
-          className={`absolute top-0 left-0 origin-top-left`} />
+          className={`absolute top-0 left-0 origin-top-left`}
+        />
 
         <div
           style={{
@@ -61,7 +64,8 @@ export const Spotlight = ({
             width: `${smallWidth}px`,
             height: `${height}px`,
           }}
-          className={`absolute top-0 left-0 origin-top-left`} />
+          className={`absolute top-0 left-0 origin-top-left`}
+        />
       </motion.div>
       <motion.div
         animate={{
@@ -73,7 +77,8 @@ export const Spotlight = ({
           repeatType: "reverse",
           ease: "easeInOut",
         }}
-        className="absolute top-0 right-0 w-screen h-screen z-40 pointer-events-none">
+        className="absolute top-0 right-0 w-screen h-screen z-40 pointer-events-none"
+      >
         <div
           style={{
             transform: `translateY(${translateY}px) rotate(45deg)`,
@@ -81,7 +86,8 @@ export const Spotlight = ({
             width: `${width}px`,
             height: `${height}px`,
           }}
-          className={`absolute top-0 right-0`} />
+          className={`absolute top-0 right-0`}
+        />
 
         <div
           style={{
@@ -90,7 +96,8 @@ export const Spotlight = ({
             width: `${smallWidth}px`,
             height: `${height}px`,
           }}
-          className={`absolute top-0 right-0 origin-top-right`} />
+          className={`absolute top-0 right-0 origin-top-right`}
+        />
 
         <div
           style={{
@@ -99,8 +106,9 @@ export const Spotlight = ({
             width: `${smallWidth}px`,
             height: `${height}px`,
           }}
-          className={`absolute top-0 right-0 origin-top-right`} />
+          className={`absolute top-0 right-0 origin-top-right`}
+        />
       </motion.div>
-    </motion.div>)
+    </motion.div>
   );
 };
